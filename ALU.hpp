e7f240a532272b7b11ff2ad79ca2bebfd6046608
Thiago Vieira <thiagovieiraas@gmail.com>
2013-12-03 21:57:48 -0200

    Adicionando arquivos

---
+++ ALU.hpp
@@ -0,0 +1,14 @@
+#ifndef _ALU_hpp
+#define _ALU_hpp
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <iomanip>
+#include <string>
+#include <cstring>
+using namespace std;
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <cctype>
+#include <string>
+#include <deque>
+using namespace std;
+
+int main()
+{
+	
+	return 0;
+}
