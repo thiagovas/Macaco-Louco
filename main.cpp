cbaa98e13de6b21a360650d7078a19235cf6a736
guilhermeleobas <guilhermeleobas@gmail.com>
2013-12-05 22:42:25 -0200

    não alterem a main

--- main.cpp
+++ main.cpp
@@ -12,38 +12,14 @@
 #include <cctype>
 #include <string>
 #include <deque>
-<<<<<<< HEAD
 #include "InstructionMemory.hpp"
-
-=======
 #include <fstream>
->>>>>>> ce3b11b36d7d6039e1594f81b8986e1631356bb9
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-<<<<<<< HEAD
 
-	InstructionMemory m;
-	ifstream input (argv[1]);
 
-	m.init (input);
-
-=======
-	ios::sync_with_stdio(false);
-	if(argc != 4)
-	{
-		cout << "Siga o padrao: ./<exec> <arquivo com instrucoes> <arquivo com dados> <arquivo de saida>" << endl;
-		return 1;
-	}
-	
-	fstream output, inputInstructions, inputData;
-	InstructionMemory im;
-	DataMemory dm;
-		
-	
-	
-	
->>>>>>> ce3b11b36d7d6039e1594f81b8986e1631356bb9
 	return 0;
 }
