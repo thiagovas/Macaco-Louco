b63225c08ce8e3b1c22476012c08c1bc5966302f
guilhermeleobas <guilhermeleobas@gmail.com>
2013-12-06 17:51:25 -0200

    adicionei o main.cpp

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <cctype>
+#include <string>
+#include <deque>
+#include "InstructionMemory.hpp"
+#include "DataMemory.hpp"
+#include "Control.hpp"
+#include "ALU.hpp"
+#include "Register.hpp"
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	ios::sync_with_stdio(false);
+	if(argc != 4)
+	{
+		cout << "Siga o padrão: ./<exec> <Arquivo com dados> <Arquivo com instruções> <Arquivo de saida>" << endl;
+		return 1;
+	}
+	
+	ifstream fData, fInstructions;
+	fstream fOutput;
+	InstructionMemory im;
+	DataMemory dm;
+	Register reg;
+	Control controle;
+    int instructionPos = 0;
+
+	fData.open(argv[1]);
+	fInstructions.open(argv[2]);
+	fOutput.open(argv[3], std::fstream::out);
+	
+	im.Init(fInstructions);
+	dm.Init(fData);
+	
+	while(instructionPos < im.GetNumInstructions())
+	{
+		
+	}
+
+	/* Finalizando tudo. Fechando streams e limpando tudo. */
+	fData.close();
+	fInstructions.close();
+	fOutput.close();
+
+	return 0;
+}
+
